07893c973a0f9583b1406273dddeb8a15b8b039e
Aditi Joshi <aditi6301@gmail.com>
2021-04-09 22:01:39 +0530

    upload images

--- branapps.sql
+++ branapps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2021 at 04:01 PM
+-- Generation Time: Apr 09, 2021 at 06:30 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -38,17 +38,26 @@ CREATE TABLE `listing` (
   `Release_date` date DEFAULT NULL,
   `min_cost` varchar(30) DEFAULT NULL,
   `max_cost` varchar(30) DEFAULT NULL,
-  `link` varchar(30) DEFAULT NULL
+  `link` varchar(30) DEFAULT NULL,
+  `image` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `listing`
 --
 
-INSERT INTO `listing` (`listing_no`, `user_id`, `Type`, `Title`, `genre`, `starcast`, `synopsis`, `Release_date`, `min_cost`, `max_cost`, `link`) VALUES
-(7, 2, 'Out_Film', 'mj', 'nn', 'nn', 'nn', '2021-04-10', 'n', 'nn', 'w'),
-(8, 2, 'In_Film', 'mj', 'nn', 'nn', 'nn', '2021-04-10', 'n', 'nn', 'wuu'),
-(9, 2, 'In_Film', 'mj', 'nn', 'nn', 'nn', '2021-04-10', 'n', 'nn', 'wuu');
+INSERT INTO `listing` (`listing_no`, `user_id`, `Type`, `Title`, `genre`, `starcast`, `synopsis`, `Release_date`, `min_cost`, `max_cost`, `link`, `image`) VALUES
+(7, 2, 'Out_Film', 'mj', 'nn', 'nn', 'nn', '2021-04-10', 'n', 'nn', 'w', NULL),
+(8, 2, 'In_Film', 'mj', 'nn', 'nn', 'nn', '2021-04-10', 'n', 'nn', 'wuu', NULL),
+(9, 2, 'In_Film', 'mj', 'nn', 'nn', 'nn', '2021-04-10', 'n', 'nn', 'wuu', NULL),
+(10, 2, 'Out_Film', 'mj', 'abc', 'abc', 'nac', '2021-05-08', '123', '123', 'www.', ''),
+(11, 2, 'Out_Film', 'mj', 'abc', 'abc', 'nac', '2021-05-08', '123', '123', 'www.', ''),
+(12, 2, 'Out_Film', 'amj', 'thriller', 'srk', 'Rom-com', '2021-05-06', '123', '123', 'www.abc.com', NULL),
+(13, 2, 'In_Film', '', '', '', '', '0000-00-00', '', '', '', 'WIN_20201106_16_54_31_Pro.jpg'),
+(14, 2, 'Out_Film', 'abcs', 'thriller', 'srk', '123', '2021-04-22', '123', '123', 'abc.com', 'Screenshot 2020-11-10 105722.p'),
+(15, 2, 'Out_Film', 'mj', 'nn', 'srk', 'abc', '2021-04-29', '123', '123', 'www.abc.com', 'header-bg21132.jpg'),
+(16, 3, 'Out_Film', 'friends', 'comedy', 'jennifer aniston', 'abcd', '2021-04-16', '123', '455', 'abc.com', 'Blue-Background-With-Hand-Draw'),
+(17, 3, 'Out_Film', 'amj', 'thriller', 'jennifer aniston', '123', '2021-04-24', '123', '123', 'www.', 'Blue-Background-With-Hand-Draw');
 
 -- --------------------------------------------------------
 
@@ -74,7 +83,12 @@ INSERT INTO `login` (`user_id`, `Email`, `CompanyName`, `login_time`, `user_stat
 (3, 'shreyakedia149@gmail.com', 'Puma', '2021-04-09 05:49:09', 'Active'),
 (2, 'aditi6301@gmail.com', 'Puma', '2021-04-09 06:37:17', 'Active'),
 (2, 'aditi6301@gmail.com', 'Puma', '2021-04-09 07:08:13', 'Active'),
-(2, 'aditi6301@gmail.com', 'Puma', '2021-04-09 07:24:06', 'Active');
+(2, 'aditi6301@gmail.com', 'Puma', '2021-04-09 07:24:06', 'Active'),
+(2, 'aditi6301@gmail.com', 'Puma', '2021-04-09 15:08:54', 'Active'),
+(2, 'aditi6301@gmail.com', 'Puma', '2021-04-09 16:08:57', 'Active'),
+(2, 'aditi6301@gmail.com', 'Puma', '2021-04-09 16:20:52', 'Active'),
+(3, 'shreyakedia149@gmail.com', 'Puma', '2021-04-09 16:23:31', 'Active'),
+(3, 'shreyakedia149@gmail.com', 'Puma', '2021-04-09 16:28:07', 'Active');
 
 -- --------------------------------------------------------
 
@@ -135,7 +149,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `listing`
 --
 ALTER TABLE `listing`
-  MODIFY `listing_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `listing_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
